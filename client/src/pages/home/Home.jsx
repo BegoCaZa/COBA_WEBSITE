cc9e76ac5cc1b5a7cfb77fa4b13d136f74b9fea2
BegoCaZa <202084412+BegoCaZa@users.noreply.github.com>
2025-06-30 11:33:47 +0200

    update

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 const Home = () => {
 	return (
-		<>
+		<div>
+			<div>
+				<img src='/assets/images/Header/construction.png' />
+			</div>
 			<h2>home page</h2>
-		</>
+		</div>
 	);
 };
 export default Home;
