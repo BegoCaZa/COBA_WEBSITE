7381616d55eeb6a60c85e7283d0e795815bb9c17
BegoCaZa <begoxcz@hotmail.com>
2025-07-16 09:50:43 +0200

    update

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -1,16 +1,40 @@
 const ProductModel = require('../models/product.model');
 
+//CREATE PRODUCT
 const createProduct = async (req, res) => {
   const newProduct = await ProductModel.create(req.body);
   res.status(201).json(newProduct);
 };
 
+//READ ALL PRODUCTS
 const getAllProducts = async (req, res) => {
   const products = await ProductModel.find();
   res.status(200).json(products);
 };
 
+//UPDATE PRODUCT BY ID
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = await ProductModel.findByIdAndUpdate(id, req.body);
+  if (!updatedProduct) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  res.status(200).json(updatedProduct);
+};
+
+//DELETE PRODUCT BY ID
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await ProductModel.findByIdAndDelete(id);
+  if (!deletedProduct) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  res.status(204).send();
+};
+
 module.exports = {
   createProduct,
-  getAllProducts
+  getAllProducts,
+  updateProduct,
+  deleteProduct
 };
--- server/src/routes/api/products.routes.js
+++ server/src/routes/api/products.routes.js
@@ -4,5 +4,7 @@ const ProductController = require('../../controllers/products.controller');
 //api/products
 router.post('/', ProductController.createProduct);
 router.get('/', ProductController.getAllProducts);
+router.patch('/:id', ProductController.updateProduct);
+router.delete('/:id', ProductController.deleteProduct);
 
 module.exports = router;
